6468faa22bd65ddcb82eab3ff85781904ecf4305
Yufei Ge <gyfuiuc@gmail.com>
2022-08-12 16:04:38 +0800

    Delete project

--- Source/ActionRoguelike/ActionRoguelike.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ActionRoguelike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRoguelike, "ActionRoguelike" );
