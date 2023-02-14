087511e4fe3a70c34b1c65a2576ff74e087fc626
Yufei Ge <gyfuiuc@gmail.com>
2023-02-15 02:12:34 +0800

    Re-structured directory, Implement hit flash effect.

---
+++ Source/ActionRoguelike/Private/GHitFlashingTest.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GHitFlashingTest.h"
+
+AGHitFlashingTest::AGHitFlashingTest()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+	AttributeComp = CreateDefaultSubobject<UGAttributeComponent>("AttributeComp");
+
+	AttributeComp->OnHealthChanged.AddDynamic(this,&AGHitFlashingTest::HitFlash);
+}
+
+void AGHitFlashingTest::HitFlash(AActor* InstigatorActor, UGAttributeComponent* OwningComp, float NewHealth,
+	float Delta)
+{
+	if(Delta<0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit",GetWorld()->TimeSeconds);
+	}
+}
+
+
+
---
+++ Source/ActionRoguelike/Public/GHitFlashingTest.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAttributeComponent.h"
+#include "GameFramework/Actor.h"
+#include "GHitFlashingTest.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API AGHitFlashingTest : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGHitFlashingTest();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UGAttributeComponent* AttributeComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+public:
+	UFUNCTION()
+	void HitFlash(AActor* InstigatorActor, UGAttributeComponent* OwningComp, float NewHealth, float Delta);
+
+};
